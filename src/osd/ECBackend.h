32fa6c646e1b50281e963d4223be96d4225fbba5
Be_a_panda <huayusheng0912@gmail.com>
2020-06-28 12:25:19 -0500

    debug

--- src/osd/ECBackend.h
+++ src/osd/ECBackend.h
@@ -648,7 +648,8 @@ public:
     ObjectStore *store,
     CephContext *cct,
     ErasureCodeInterfaceRef ec_impl,
-    uint64_t stripe_width);
+    uint64_t stripe_width,
+    OSDService *o);
 
   /// Returns to_read replicas sufficient to reconstruct want
   int get_min_avail_to_read_shards(
