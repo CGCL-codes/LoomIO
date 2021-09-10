eab5536c6331f68920462242ebc007a5a6de7e62
Be_a_panda <huayusheng0912@gmail.com>
2021-09-10 17:19:05 +0800

    debug

--- src/os/greenfs/greenfs_main.cc
+++ src/os/greenfs/greenfs_main.cc
@@ -1,11 +1,38 @@
-#include <boost/program_options.hpp>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <boost/scoped_ptr.hpp>
+
 #include <iostream>
-#include "aio.h"
-
-#include "greenfs.h"
-
+#include <string>
 using namespace std;
-namespace po = boost::program_options;
+
+#include "osd/OSD.h"
+#include "os/ObjectStore.h"
+#include "mon/MonClient.h"
+#include "include/ceph_features.h"
+
+#include "common/config.h"
+#include "common/version.h"
+
+#include "mon/MonMap.h"
+
+#include "msg/Messenger.h"
+
+#include "common/Timer.h"
+#include "common/TracepointProvider.h"
+#include "common/ceph_argparse.h"
+
+#include "global/global_init.h"
+#include "global/signal_handler.h"
+
+#include "include/color.h"
+#include "common/errno.h"
+#include "common/pick_address.h"
+
+#include "perfglue/heap_profiler.h"
+
+#include "include/assert.h"
 
 int main(int args, char** argv){
 
