f1d252c6d5a3ade84e052f630b8e193c2a2e1675
Be_a_panda <huayusheng0912@gmail.com>
2021-09-13 10:18:41 +0800

    debug

--- src/ceph_osd.cc
+++ src/ceph_osd.cc
@@ -108,10 +108,12 @@ int main(int argc, const char **argv)
   // option, therefore we will pass it as a default argument to global_init().
   def_args.push_back("--leveldb-log=");
 
-  dout(0)<<"mydebug: into main "<<dendl;
+  // dout(0)<<"mydebug: into main "<<dendl;
 
-  // for(auto i: args)
-  //   dout(0)<<"mydebug: argv "<<string(i)<<dendl;
+  // for(int i=0;i<args.size();i++){
+  //   dout(0)<<"mydebug: argv "<<args[i]<<dendl;
+  // }
+    
 
   auto cct = global_init(&def_args, args, CEPH_ENTITY_TYPE_OSD,
 			 CODE_ENVIRONMENT_DAEMON,
