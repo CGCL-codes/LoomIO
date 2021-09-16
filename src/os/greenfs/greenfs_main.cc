9e5866dded8554e91a3002f9673a63b14704f216
Be_a_panda <huayusheng0912@gmail.com>
2021-09-16 16:12:44 +0800

    debug

--- src/os/greenfs/greenfs_main.cc
+++ src/os/greenfs/greenfs_main.cc
@@ -50,9 +50,9 @@ greenfs_global_init(&def_args, args, CEPH_ENTITY_TYPE_OSD,
 			 0, "osd_data");
 
 
-if (get_env_bool("CEPH_HEAP_PROFILER_INIT")) {
-	cout<<"CEPH_HEAP_PROFILER_INIT==1"<<std::endl;
-}
+// if (get_env_bool("CEPH_HEAP_PROFILER_INIT")) {
+// 	cout<<"CEPH_HEAP_PROFILER_INIT==1"<<std::endl;
+// }
 //ceph_heap_profiler_init();
 
 }
